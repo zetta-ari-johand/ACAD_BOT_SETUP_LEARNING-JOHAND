c3cc379ae19685438a59841a5b080784ee4ab77b
zettacamp-ari-johand <ari.johand@zettacamp.pro>
2024-01-04 08:06:45 +0700

    first commit, connect to express

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+module.exports = app;
